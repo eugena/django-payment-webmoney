4623342ee3fc09df555380e0a0a13229d48a9475
Eugena Mihailikova <eugena.mihailikova@gmail.com>
2016-07-26 13:07:45 +0500

    Compatibility with django 1.10

--- webmoney/urls.py
+++ webmoney/urls.py
@@ -1,11 +1,10 @@
 try:
-    from django.conf.urls import patterns, url
+    from django.conf.urls import url
 except ImportError:
-    from django.conf.urls.defaults import patterns, url
+    from django.conf.urls.defaults import url
 
 from webmoney.views import result
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^result/$', result, name='webmoney-result'),
-)
+]
